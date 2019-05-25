b7cdd547c0596579cab42bcb9a9792b2bd6406cc
Gabriel Pivato <pivatogabriel@gmail.com>
2019-05-25 20:07:37 -0300

    download playlist as mp3

---
+++ youtube-dl_download_playlistMP3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for n in *.mp3
+do if [[ "$n" =~ -[-_0-9a-zA-Z]{11}.mp3$ ]]
+   then echo "youtube ${n: -15: 11}" >> downloaded.txt
+   fi
+done
+
+echo "Playlist Link:"
+read url
+
+youtube-dl --download-archive downloaded.txt --no-post-overwrites -ciwx --audio-format mp3 -o "%(title)s.%(ext)s" $url
+
+rm downloaded.txt
